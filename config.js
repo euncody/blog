9400f26eee648bdb2f2392b43f4a891800948442
김은지 <dotagekeji96@gmail.com>
2025-07-30 16:00:44 +0900

    닉네임 맞춰야하나보다

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "euncody",
+    username: "licat",
     company: "-",
     position: "-",
     img: "img/user/profile-licat.png",
